57859e8ee7e7c1bb2ffae446b6f32069d30dc2f5
NathanTh3Gr3at <112683403+NathanTh3Gr3at@users.noreply.github.com>
2024-12-13 10:21:59 +0200

    Worked through course

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+final formatter = DateFormat.yMd();
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   final String id;
@@ -10,7 +21,33 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+  get formattedDate {
+    return formatter.format(date);
+  }
 
-  Expense( {required this.title, required this.amount, required this.date,required this.category})
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
       : id = uuid.v4();
+}
+
+class ExpenseBucket {
+  final Category category;
+  final List<Expense> expenses;
+
+  ExpenseBucket({required this.category, required this.expenses});
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
 }
